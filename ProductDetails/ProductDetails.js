31131e32803945e8edd9643e1ea4c635b0d6450c
Amirli Mirtalib Ulfat <Amirl_kl34@itstep.edu.az>
2024-11-26 19:09:19 +0400

    Js created

---
+++ ProductDetails/ProductDetails.js
@@ -0,0 +1,15 @@
+const searchParams = new URLSearchParams(window.location.search).get("productId")
+console.log(searchParams)
+ 
+const productId = document.cookie.split("=")[1]
+console.log(productId)
+
+
+
+
+
+
+
+
+
+
