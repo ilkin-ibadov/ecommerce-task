f88c3686622508902e199bb98543a74fe1f69112
afetism <aismayilova1215@gmail.com>
2024-11-26 23:33:33 +0400

    intro js code

--- ProductDetails/ProductDetails.js
+++ ProductDetails/ProductDetails.js
@@ -1,11 +1,23 @@
-const searchParams = new URLSearchParams(window.location.search).get("productId")
-console.log(searchParams)
- 
-const productId = document.cookie.split("=")[1]
-console.log(productId)
+const productId = new URLSearchParams(window.location.search).get("id")
+const getProductsDetails=async (productId)=>
+{
+    try {
+        const response=await fetch(`http://localhost:3000/api/products/${productId}`);
+        if(!response.ok)
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        const data=await response.json();
+        console.log(data);
 
-
-
+        document.getElementById('product-name').textContent=data.product.title;
+        document.getElementById('product-price').textContent=`${parseFloat(data.product.price).toFixed(2)}${data.product.currency}`
+        document.getElementById('product-description').textContent=data.product.description;
+        if(data.product.stock>0)
+            
+    } catch (error) {
+        console.error('Xeta bas verdi:',error);
+    }
+}
+getProductsDetails(productId);
 
 
 
